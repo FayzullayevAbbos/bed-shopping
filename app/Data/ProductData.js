0f4892289f02f116d841fd8e85e7940d134a2c9c
FayzullayevAbbos <abbosfayzullyev202@gmail.com>
2024-05-30 09:16:00 +0500

    global installation datas , languages , redax

---
+++ app/Data/ProductData.js
@@ -0,0 +1,559 @@
+import WinterCollection1 from "../../public/images/WinterCollection1"
+import WinterCollection3 from "../../public/images/winter-collection3.jpg"
+import WinterCollection4 from "../../public/images/winter-collection4.jpg"
+import WinterCollection2 from "../../public/images/winter-collection2.jpg"
+import WinterCollection5 from "../../public/images/winter-collection5.jpg"
+import WinterCollection6 from "../../public/images/winter-collection6.jpg"
+import WinterCollection7 from "../../public/images/winter-collection7.jpg"
+import WinterCollection8 from "../../public/images/winter-collection8.jpg"
+import WinterCollection9 from "../../public/images/winter-collection9.jpg"
+import WinterCollection10 from "../../public/images/winter-collection10.jpg"
+import AutumnCollection1 from "../../public/images/autumCollection1.jpg"
+import AutumnCollection2 from "../../public/images/autumCollection2.jpg"
+import AutumnCollection3 from "../../public/images/autumCollection3.jpg"
+import AutumnCollection4 from "../../public/images/autumCollection4.jpg"
+import AutumnCollection5 from "../../public/images/autumCollection5.jpg"
+import AutumnCollection6 from "../../public/images/autumCollection6.jpg"
+import AutumnCollection7 from "../../public/images/autumCollection7.jpg"
+import AutumnCollection8 from "../../public/images/autumCollection8.jpg"
+import AutumnCollection9 from "../../public/images/autumCollection9.jpg"
+import AutumnCollection10 from "../../public/images/autumCollection10.jpg"
+import SummerCollection1 from "../../public/images/SummerCollection1.jpg"
+import SummerCollection2 from "../../public/images/SummerCollection2.jpg"
+import SummerCollection3 from "../../public/images/SummerCollection3.jpg"
+import SummerCollection4 from "../../public/images/SummerCollection4.jpg"
+import SummerCollection5 from "../../public/images/SummerCollection5.jpg"
+import SummerCollection6 from "../../public/images/SummerCollection6.jpg"
+import SummerCollection7 from "../../public/images/SummerCollection7.jpg"
+import SummerCollection8 from "../../public/images/SummerCollection8.jpg"
+import SummerCollection9 from "../../public/images/SummerCollection9.jpg"
+import SummerCollection10 from "../../public/images/SummerCollection10.jpg"
+const CollectionData = [
+    {
+        id:1,
+        collectionName:"main-collectionName1",
+        name:"product-name1",
+        img: WinterCollection1,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:2,
+        collectionName:"main-collectionName1",
+        name:"product-name2",
+        img: WinterCollection2,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:3,
+        collectionName:"main-collectionName1",
+        name:"product-name3",
+        img:WinterCollection3,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:4,
+        collectionName:"main-collectionName1",
+        name:"product-name4",
+        img: WinterCollection4,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:5,
+        collectionName:"main-collectionName1",
+        name:"product-name5",
+        img: WinterCollection5,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:6,
+        collectionName:"main-collectionName1",
+        name:"product-name6",
+        img: WinterCollection6,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:7,
+        collectionName:"main-collectionName1",
+        name:"product-name7",
+        img: WinterCollection7,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:8,
+        collectionName:"main-collectionName1",
+        name:"product-name8",
+        img: WinterCollection8,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:9,
+        collectionName:"main-collectionName1",
+        name:"product-name9",
+        img: WinterCollection9,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:10,
+        collectionName:"main-collectionName1",
+        name:"product-name10",
+        img: WinterCollection10,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:11,
+        collectionName:"main-collectionName2",
+        name:"product-name11",
+        img:AutumnCollection1,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:12,
+        collectionName:"main-collectionName2",
+        name: "product-name12",
+        img:AutumnCollection2,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:13,
+        collectionName:"main-collectionName2",
+        name:"product-name13",
+        img:AutumnCollection3,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:14,
+        collectionName:"main-collectionName2",
+        name:"product-name14",
+        img:AutumnCollection4,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:15,
+        collectionName:"main-collectionName2",
+        name:"product-name15",
+        img:AutumnCollection5,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:16,
+        collectionName:"main-collectionName2",
+        name:"product-name16",
+        img:AutumnCollection6,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:17,
+        collectionName:"main-collectionName2",
+        name:"product-name17",
+        img:AutumnCollection7,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:18,
+        collectionName:"main-collectionName2",
+        name:"product-name18",
+        img:AutumnCollection8,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:19,
+        collectionName:"main-collectionName2",
+        name:"product-name19",
+        img:AutumnCollection9,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:20,
+        collectionName:"main-collectionName2",
+        name:"product-name20",
+        img:AutumnCollection10,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:21,
+        collectionName: "main-collectionName3",
+        name:"product-name21",
+        img:SummerCollection1,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:22,
+        collectionName: "main-collectionName3",
+        name:"product-name22",
+        img:SummerCollection2,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:23,
+        collectionName: "main-collectionName3",
+        name:"product-name23",
+        img:SummerCollection3,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:24,
+        collectionName: "main-collectionName3",
+        name:"product-name24",
+        img:SummerCollection4,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:25,
+        collectionName: "main-collectionName3",
+        name:"product-name25",
+        img:SummerCollection5,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:26,
+        collectionName: "main-collectionName3",
+        name:"product-name26",
+        img:SummerCollection6,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:27,
+        collectionName: "main-collectionName3",
+        name:"product-name27",
+        img:SummerCollection7,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:28,
+        collectionName: "main-collectionName3",
+        name:"product-name28",
+        img:SummerCollection8,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:29,
+        collectionName: "main-collectionName3",
+        name:"product-name29",
+        img:SummerCollection9,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:30,
+        collectionName: "main-collectionName3",
+        name:"product-name30",
+        img:SummerCollection10,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:31,
+        collectionName: "main-collectionName4",
+        name:"product-name4",
+        img:SummerCollection10,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:32,
+        collectionName: "main-collectionName4",
+        name:"product-name12",
+        img:AutumnCollection1,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:33,
+        collectionName: "main-collectionName4",
+        name:"product-name23",
+        img:SummerCollection7,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:34,
+        collectionName: "main-collectionName4",
+        name:"product-name9",
+        img:AutumnCollection5,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+    {
+        id:35,
+        collectionName: "main-collectionName4",
+        name:"product-name30",
+        img:AutumnCollection9,
+        productDetails: {
+            description:"product-descr",
+            material:"material-item1",
+            pillow_case: "material-item2",
+            bed_sheet: "material-item3",
+            duvet_cover:"material-item4",
+            size:"material-item5",
+            manufacturer: "material-item6"
+        }
+    },
+]
+
+export  default CollectionData
